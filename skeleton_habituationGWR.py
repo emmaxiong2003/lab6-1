6903db8c5b75008b3a70efc05ee80e60df1b9e51
emmaxiong2003 <68303086+emmaxiong2003@users.noreply.github.com>
2020-07-16 14:38:55 -0400

    Update skeleton_habituationGWR.py

--- skeleton_habituationGWR.py
+++ skeleton_habituationGWR.py
@@ -16,36 +16,48 @@ import matplotlib.pyplot as plt
 ###############################
 # TODO: Set a variable called stv to 4, this will define 
 #        the weight of the connection from input to output
-# stv = 
+stv = 4
 
 dec=0.7 # set weight decrement for habituation
 
 pls=[0, 0, 1, 0, 0] # set up a pulse
 
 # TODO: then create a list of 6 pulses, called x, to use for input
-# x = 
+x = pls*6 + [0]*10 + pls*6 # rest period added
 
 v = stv # Set connection weight to start weight value
 
-###############################
+forgetflag=True # if true, allows model to simulate process
+                # of forgetting the new association 
+                # if pulse is not felt for a while, forget 
+                # the association
+############################### 
 # Set up and run simulation
 ###############################
 
 # TODO: Find the length of the input list (x)
 #        and assign its value to a new variable, nTs
 #        (which tracks the number of Time Steps)
-# nTs = 
+nTs = len(x)
 
 # TODO: Create a numpy array named y, a 1D vector
 #        (row vector) padded with zeros, with one
 #        element per each time step
-# y = 
+y = np.zeros((1,nTs))
+
 
 # TODO: use a for-loop to iterate 
 #        through each time step in 
 #        the input series and calculate
 #        the output at each time step. Ex:
-# for ...
+for t in range(nTs):
+    y[0,t]=v*x[t]
+    
+    if x[t]>0:
+        v*=dec 
+    
+    if t>3 and 0==sum(x[t-4:t]) and v<stv and forgetflag:
+        v+=(stv-v)*.05 # forget the association if it doesn't happen for a while
 
 #     then indent 4 spaces and write the equation that
 #     describes how each input value in the vector x is 
@@ -75,6 +87,6 @@ ax2.plot(y[0]) #, color='white',  antialiased=False, edgecolors='black', linewid
 ax2.set_xlabel('Time step')
 ax2.set_ylabel('Output')
 ax2.set_xlim(0, nTs)
-ax2.set_ylim(0, stv+0.5)
+# ax2.set_ylim(0, stv+0.5)
 
 plt.show()
